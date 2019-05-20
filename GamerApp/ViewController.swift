05a03c84844a5a9c511e18a46c6dcfa6ab45a45e
David Jorge <david.jorge@appconcept.es>
2019-05-20 11:18:53 +0200

    Initial Commit

---
+++ GamerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GamerApp
+//
+//  Created by David Jorge on 20/05/2019.
+//  Copyright © 2019 David Jorge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
